1176480987315a076d0826bb01e8c35ae6306caf
dragonkid <idragonkid@gmail.com>
2026-04-22 15:38:34 +0800

    exp: vol-adaptive peak profit giveback (tighter in low vol, wider in high vol)

--- strategy.py
+++ strategy.py
@@ -329,7 +329,8 @@ class Strategy:
                     self.peak_pnl[symbol] = max(prev_peak, pos_pnl)
                     if self.peak_pnl[symbol] > PEAK_PROFIT_MIN:
                         giveback = self.peak_pnl[symbol] - pos_pnl
-                        if giveback > self.peak_pnl[symbol] * PEAK_PROFIT_GIVEBACK:
+                        adaptive_giveback = PEAK_PROFIT_GIVEBACK * max(0.7, min(1.5, vol_ratio ** 0.5))
+                        if giveback > self.peak_pnl[symbol] * adaptive_giveback:
                             target = 0.0
 
                 flip_bearish = bear_votes >= FLIP_MIN_VOTES and trend_bear
