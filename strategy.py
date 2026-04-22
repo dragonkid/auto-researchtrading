b3e8020e58c390f94588cad8567e9ae18c10dd15
dragonkid <idragonkid@gmail.com>
2026-04-22 20:41:51 +0800

    Revert "exp: reduce giveback age loosening 0.15->0.10 to lock in profits tighter on older positions"
    This reverts commit ffc98c7bd2f35020915006152effb41f9bffbabe.

--- strategy.py
+++ strategy.py
@@ -303,7 +303,7 @@ class Strategy:
                         age_factor = min(bars_held / 12.0, 1.0)
                         sideways_giveback_tighten = max(0.0, 1.0 - abs(ret_long) / MEANREV_TREND_THRESHOLD)
                         r2_loosen = 0.08 * linreg_r2
-                        adaptive_giveback = 0.25 - 0.05 * sideways_giveback_tighten + 0.10 * age_factor + r2_loosen
+                        adaptive_giveback = 0.25 - 0.05 * sideways_giveback_tighten + 0.15 * age_factor + r2_loosen
                         if giveback > self.peak_pnl[symbol] * adaptive_giveback:
                             target = 0.0
 
