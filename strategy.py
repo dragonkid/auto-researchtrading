d5729a1d7face1691253aef8b7a6ba357e103012
dragonkid <idragonkid@gmail.com>
2026-04-23 11:03:59 +0800

    Revert "exp: asymmetric peak-profit giveback for longs vs shorts"
    This reverts commit b32d317bc766f99d9018de2098c2e32f04f0fd64.

--- strategy.py
+++ strategy.py
@@ -59,8 +59,7 @@ RSI_YOUNG_OS_WIDEN = 4.0
 
 # Peak-profit trailing exit
 PEAK_PROFIT_MIN_BASE = 0.025
-PEAK_PROFIT_GIVEBACK_LONG = 0.28
-PEAK_PROFIT_GIVEBACK_SHORT = 0.20
+PEAK_PROFIT_GIVEBACK = 0.25
 
 # Sizing multipliers
 BASE_POSITION_SIZE = 0.115
@@ -365,8 +364,7 @@ class Strategy:
                     adaptive_peak_min = PEAK_PROFIT_MIN_BASE * max(0.6, min(2.0, vol_ratio ** 0.5))
                     if self.peak_pnl[symbol] > adaptive_peak_min:
                         giveback = self.peak_pnl[symbol] - pos_pnl
-                        giveback_pct = PEAK_PROFIT_GIVEBACK_SHORT if current_pos < 0 else PEAK_PROFIT_GIVEBACK_LONG
-                        if giveback > self.peak_pnl[symbol] * giveback_pct:
+                        if giveback > self.peak_pnl[symbol] * PEAK_PROFIT_GIVEBACK:
                             target = 0.0
 
                 flip_bearish = bear_votes >= FLIP_MIN_VOTES and trend_bear
