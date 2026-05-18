209c13573a75d7607c831df17916ce75879c2d98
dragonkid <idragonkid@gmail.com>
2026-05-19 01:28:20 +0800

    Revert "exp: vol-conditioned EMA spread threshold (0.1% min spread only when vol_ratio<0.8)"
    This reverts commit 8789e1cd0028f8dedd066377db58d7a5a715f449.

--- strategy.py
+++ strategy.py
@@ -221,10 +221,8 @@ class Strategy:
 
             ema_fast_arr = ema(closes[-(EMA_SLOW+10):], EMA_FAST)
             ema_slow_arr = ema(closes[-(EMA_SLOW+10):], EMA_SLOW)
-            ema_spread = (ema_fast_arr[-1] - ema_slow_arr[-1]) / ema_slow_arr[-1]
-            ema_min_spread = 0.001 if vol_ratio < 0.8 else 0.0
-            ema_bull = ema_spread > ema_min_spread
-            ema_bear = ema_spread < -ema_min_spread
+            ema_bull = ema_fast_arr[-1] > ema_slow_arr[-1]
+            ema_bear = ema_fast_arr[-1] < ema_slow_arr[-1]
 
             rsi_trend_str = min(abs(ret_long) / RSI_TREND_BIAS_DECAY, 1.0)
             adaptive_rsi_period = int(round(6 + 2 * rsi_trend_str))
