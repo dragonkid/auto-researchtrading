6b71bd5bb453d1333968aa21fe716858b7f8fb5e
dragonkid <idragonkid@gmail.com>
2026-05-23 22:20:05 +0800

    Revert "exp: require 3 bars held before position flip triggers"
    This reverts commit 74929711d15680b324cb98811c04adb70f20263e.

--- strategy.py
+++ strategy.py
@@ -375,9 +375,9 @@ class Strategy:
 
                 flip_bearish = bear_votes >= FLIP_MIN_VOTES and trend_bear
                 flip_bullish = bull_votes >= FLIP_MIN_VOTES and trend_bull
-                if current_pos > 0 and flip_bearish and not in_cooldown and bars_held >= 3:
+                if current_pos > 0 and flip_bearish and not in_cooldown:
                     target = -size
-                elif current_pos < 0 and flip_bullish and not in_cooldown and bars_held >= 3:
+                elif current_pos < 0 and flip_bullish and not in_cooldown:
                     target = size
 
             if abs(target - current_pos) > 1.0:
