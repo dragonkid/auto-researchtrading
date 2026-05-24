71b574c4a227a20bb6d1308be46ca9621f6a4d9f
dragonkid <idragonkid@gmail.com>
2026-05-24 08:22:04 +0800

    Revert "exp: asymmetric Donchian (close+margin for bull, intrabar lows for bear)"
    This reverts commit 4647986d100b8058d42f14aa302d2ae5ca63700e.

--- strategy.py
+++ strategy.py
@@ -200,9 +200,9 @@ class Strategy:
             vol_breakout_bear = vb_short > realized_vol and ret_vshort < -dyn_threshold * 0.20
 
             donchian_high = np.max(closes[-(DONCHIAN_PERIOD+1):-1])
-            donchian_low = np.min(bd.history["low"].values[-(DONCHIAN_PERIOD+1):-1])
+            donchian_low = np.min(closes[-(DONCHIAN_PERIOD+1):-1])
             donchian_bull = mid > donchian_high * 1.003
-            donchian_bear = mid < donchian_low
+            donchian_bear = mid < donchian_low * 0.997
 
             bull_votes = sum([mom_bull, vshort_bull, ema_bull, rsi_bull, macd_bull, vol_breakout_bull, linreg_bull, donchian_bull, slope_bull])
             bear_votes = sum([mom_bear, vshort_bear, ema_bear, rsi_bear, macd_bear, vol_breakout_bear, linreg_bear, donchian_bear, slope_bear])
