5e73d9ac43e24fd93f153a476dc64e1c43acae3a
dragonkid <idragonkid@gmail.com>
2026-05-26 01:10:54 +0800

    Revert "exp: increase noise zone margin 1.5 to 1.7pt (hold profitable positions longer in calm)"
    This reverts commit 5b18d5a47d5dd1e8efe4989be7c44de1b567c75b.

--- strategy.py
+++ strategy.py
@@ -198,7 +198,7 @@ class Strategy:
                     _uw = 1.5 * min(1.0, abs(pos_pnl) / 0.012)
                     effective_ob, effective_os = effective_ob + _uw, effective_os - _uw
                 if vol_ratio < 0.55 and pos_pnl > 0.01 and ((current_pos > 0 and _lr.slope > 0) or (current_pos < 0 and _lr.slope < 0)):
-                    effective_ob, effective_os = effective_ob + 1.7, effective_os - 1.7
+                    effective_ob, effective_os = effective_ob + 1.5, effective_os - 1.5
                 if current_pos > 0 and rsi_exit > effective_ob:
                     target = 0.0
                 elif current_pos < 0 and rsi_exit < effective_os:
