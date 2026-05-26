c842814118a59607f845d3d59756dbfa67e9b73a
dragonkid <idragonkid@gmail.com>
2026-05-26 23:24:09 +0800

    Revert "exp: widen mean-rev RSI 49/51->47/53 + require opposing vote support (architectural multi-variable)"
    This reverts commit b9a28d8e1fb7695d5e65d538c28602598d7b827d.

--- strategy.py
+++ strategy.py
@@ -85,8 +85,8 @@ TREND_GATE_MED_WEIGHT_SIDEWAYS = 0.85
 TREND_GATE_MED_WEIGHT_BASE = 0.70
 TREND_GATE_DEADZONE = 0.013
 MEANREV_TREND_THRESHOLD = 0.05
-MEANREV_RSI_OVERSOLD = 47
-MEANREV_RSI_OVERBOUGHT = 53
+MEANREV_RSI_OVERSOLD = 49
+MEANREV_RSI_OVERBOUGHT = 51
 
 # Vote / cooldown
 DONCHIAN_PERIOD = 13
@@ -177,7 +177,7 @@ class Strategy:
                     target = size
                 elif bear_votes >= MIN_VOTES and (self.smoothed_trend[symbol] < 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bear_votes > bull_votes)):
                     target = -size
-                elif abs(ret_long) < MEANREV_TREND_THRESHOLD and ((rsi < MEANREV_RSI_OVERSOLD and bear_votes <= 1) or (rsi > MEANREV_RSI_OVERBOUGHT and bull_votes <= 1)):
+                elif abs(ret_long) < MEANREV_TREND_THRESHOLD and (rsi < MEANREV_RSI_OVERSOLD or rsi > MEANREV_RSI_OVERBOUGHT):
                     target = size if rsi < MEANREV_RSI_OVERSOLD else -size
             elif current_pos != 0:
                 vol_exit_blend = max(0.0, min(1.0, (vol_ratio - RSI_EXIT_VOL_LOW) / (RSI_EXIT_VOL_HIGH - RSI_EXIT_VOL_LOW)))
