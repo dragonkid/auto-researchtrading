20ed2b3d432cf957b1ec4709cdf359d095315420
dragonkid <idragonkid@gmail.com>
2026-05-27 00:21:02 +0800

    Revert "exp: 2-bar vote persistence for entries (layer 3 hysteresis on entry decision)"
    This reverts commit 8dc5a2b781241c25f010f6f7e2c5db9801e2f40b.

--- strategy.py
+++ strategy.py
@@ -109,7 +109,6 @@ class Strategy:
         self.entry_prices, self.exit_bar, self.peak_pnl, self.entry_bar = {}, {}, {}, {}
         self.bar_count = 0
         self.smoothed_trend = {}
-        self.last_bull_votes, self.last_bear_votes = {}, {}
 
     def on_bar(self, bar_data, portfolio):
         signals = []
@@ -173,12 +172,10 @@ class Strategy:
             current_pos = portfolio.positions.get(symbol, 0.0)
             target = current_pos
 
-            _last_bull, _last_bear = self.last_bull_votes.get(symbol, 0), self.last_bear_votes.get(symbol, 0)
-            self.last_bull_votes[symbol], self.last_bear_votes[symbol] = bull_votes, bear_votes
             if current_pos == 0 and not in_cooldown:
-                if bull_votes >= MIN_VOTES and _last_bull >= MIN_VOTES and (self.smoothed_trend[symbol] > 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bull_votes > bear_votes)):
+                if bull_votes >= MIN_VOTES and (self.smoothed_trend[symbol] > 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bull_votes > bear_votes)):
                     target = size
-                elif bear_votes >= MIN_VOTES and _last_bear >= MIN_VOTES and (self.smoothed_trend[symbol] < 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bear_votes > bull_votes)):
+                elif bear_votes >= MIN_VOTES and (self.smoothed_trend[symbol] < 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bear_votes > bull_votes)):
                     target = -size
                 elif abs(ret_long) < MEANREV_TREND_THRESHOLD and (rsi < MEANREV_RSI_OVERSOLD or rsi > MEANREV_RSI_OVERBOUGHT):
                     target = size if rsi < MEANREV_RSI_OVERSOLD else -size
