74976e07bf5fe8f93b7c6e657b77b4002bb7e689
dragonkid <idragonkid@gmail.com>
2026-05-27 15:44:00 +0800

    Revert "branch: smoothed pos_pnl peak with alpha 0.85/0.15 (faster smoothing)"
    This reverts commit e49b6f2d3331bebc7bf77b4af4bd4f2a6e6919f4.

--- strategy.py
+++ strategy.py
@@ -109,7 +109,7 @@ class Strategy:
         self.entry_prices, self.exit_bar, self.peak_pnl, self.entry_bar = {}, {}, {}, {}
         self.bar_count = 0
         self.smoothed_trend = {}
-        self.smoothed_pos_pnl = {}
+        self.recent_pnls = {}
 
     def on_bar(self, bar_data, portfolio):
         signals = []
@@ -214,8 +214,10 @@ class Strategy:
                     target = 0.0
 
                 if target != 0:
-                    self.smoothed_pos_pnl[symbol] = 0.85 * pos_pnl + 0.15 * self.smoothed_pos_pnl.get(symbol, pos_pnl)
-                    self.peak_pnl[symbol] = max(self.peak_pnl.get(symbol, 0.0), self.smoothed_pos_pnl[symbol])
+                    _rp = self.recent_pnls.get(symbol, [pos_pnl, pos_pnl])[-2:] + [pos_pnl]
+                    self.recent_pnls[symbol] = _rp
+                    _median_pnl = sorted(_rp)[1]
+                    self.peak_pnl[symbol] = max(self.peak_pnl.get(symbol, 0.0), _median_pnl)
                     if self.peak_pnl[symbol] > PEAK_PROFIT_MIN_BASE * max(0.6, min(2.0, vol_ratio ** 0.5)) and self.peak_pnl[symbol] - pos_pnl > self.peak_pnl[symbol] * PEAK_PROFIT_GIVEBACK:
                         target = 0.0
 
@@ -225,7 +227,7 @@ class Strategy:
             if abs(target - current_pos) > 1.0:
                 signals.append(Signal(symbol=symbol, target_position=target))
                 if target == 0:
-                    for _d in (self.entry_prices, self.peak_pnl, self.entry_bar, self.smoothed_pos_pnl):
+                    for _d in (self.entry_prices, self.peak_pnl, self.entry_bar, self.recent_pnls):
                         _d.pop(symbol, None)
                     self.exit_bar[symbol] = self.bar_count
                 elif current_pos == 0 or (target > 0 and current_pos < 0) or (target < 0 and current_pos > 0):
