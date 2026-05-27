aa185ec95052df2c4a6fd13025c0b42a11a88e9d
dragonkid <idragonkid@gmail.com>
2026-05-27 20:39:31 +0800

    Revert "branch: revert smoothed peak, smoothed_trend alpha 0.65->0.50 (more persistence) + bull-exit removal"
    This reverts commit 054946aa0009a7a9e5fc8e2a4036c83957d59a36.

--- strategy.py
+++ strategy.py
@@ -158,7 +158,7 @@ class Strategy:
 
             cooldown_trend_strength = min(abs(ret_long) / COOLDOWN_TREND_DECAY, 1.0)
             trend_avg = (TREND_GATE_MED_WEIGHT_SIDEWAYS - (TREND_GATE_MED_WEIGHT_SIDEWAYS - TREND_GATE_MED_WEIGHT_BASE) * cooldown_trend_strength) * ((closes[-1] - closes[-MED2_WINDOW]) / closes[-MED2_WINDOW]) + ((1.0 - TREND_GATE_MED_WEIGHT_SIDEWAYS) + (TREND_GATE_MED_WEIGHT_SIDEWAYS - TREND_GATE_MED_WEIGHT_BASE) * cooldown_trend_strength) * ret_long
-            self.smoothed_trend[symbol] = 0.50 * trend_avg + 0.50 * self.smoothed_trend.get(symbol, trend_avg)
+            self.smoothed_trend[symbol] = 0.65 * trend_avg + 0.35 * self.smoothed_trend.get(symbol, trend_avg)
 
             in_cooldown = (self.bar_count - self.exit_bar.get(symbol, -999)) < COOLDOWN_BARS * cooldown_trend_strength
 
@@ -216,7 +216,8 @@ class Strategy:
                     target = 0.0
 
                 if target != 0:
-                    self.peak_pnl[symbol] = max(self.peak_pnl.get(symbol, 0.0), pos_pnl)
+                    self.peak_pnl_raw[symbol] = max(self.peak_pnl_raw.get(symbol, 0.0), pos_pnl)
+                    self.peak_pnl[symbol] = 0.7 * self.peak_pnl_raw[symbol] + 0.3 * self.peak_pnl.get(symbol, self.peak_pnl_raw[symbol])
                     if self.peak_pnl[symbol] > PEAK_PROFIT_MIN_BASE * max(0.6, min(2.0, vol_ratio ** 0.5)) and self.peak_pnl[symbol] - pos_pnl > self.peak_pnl[symbol] * PEAK_PROFIT_GIVEBACK:
                         target = 0.0
 
