8c93f607306c9cc86bc4ad1d2ee655b059ce9491
dragonkid <idragonkid@gmail.com>
2026-05-27 12:34:49 +0800

    Revert "exp: branch tag deadzone entries as mr (tight exits)"
    This reverts commit 06b0fbc03e7f9f001d4ede7d5b5e0318733df0b0.

--- strategy.py
+++ strategy.py
@@ -175,14 +175,10 @@ class Strategy:
             _new_path = None
 
             if current_pos == 0 and not in_cooldown:
-                if bull_votes >= MIN_VOTES and self.smoothed_trend[symbol] > 0:
+                if bull_votes >= MIN_VOTES and (self.smoothed_trend[symbol] > 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bull_votes > bear_votes)):
                     target, _new_path = size, 'trend'
-                elif bear_votes >= MIN_VOTES and self.smoothed_trend[symbol] < 0:
+                elif bear_votes >= MIN_VOTES and (self.smoothed_trend[symbol] < 0 or (abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bear_votes > bull_votes)):
                     target, _new_path = -size, 'trend'
-                elif abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bull_votes >= MIN_VOTES and bull_votes > bear_votes:
-                    target, _new_path = size, 'mr'
-                elif abs(self.smoothed_trend[symbol]) < TREND_GATE_DEADZONE and bear_votes >= MIN_VOTES and bear_votes > bull_votes:
-                    target, _new_path = -size, 'mr'
                 elif abs(ret_long) < MEANREV_TREND_THRESHOLD and (rsi < MEANREV_RSI_OVERSOLD or rsi > MEANREV_RSI_OVERBOUGHT):
                     target, _new_path = (size if rsi < MEANREV_RSI_OVERSOLD else -size), 'mr'
             elif current_pos != 0:
