5848b664dbc42d1dcbbb4b36e2ff88808e4f0e1a
dragonkid <idragonkid@gmail.com>
2026-05-30 10:39:58 +0800

    Revert "branch step 7: SIZE 0.055->0.060 (balance DD vs raw_composite)"
    This reverts commit c21793bc8d06d9fdb26e42133d38e44b43eb385b.

--- strategy.py
+++ strategy.py
@@ -51,7 +51,7 @@ PEAK_PROFIT_MIN_BASE = 0.025
 PEAK_PROFIT_GIVEBACK = 0.25
 
 # Sizing multipliers
-BASE_POSITION_SIZE = 0.060  # reduced from 0.067 to control DD with vote-proportional sizing
+BASE_POSITION_SIZE = 0.055  # reduced from 0.067 to control DD with vote-proportional sizing
 CALM_BOOST_MAX = 0.8
 SIDEWAYS_BOOST_MAX = 0.50
 CROSS_ASSET_FIXED_BOOST = 0.15
