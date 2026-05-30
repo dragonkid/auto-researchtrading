0f8e98dcd4b40d8279aecfb3eb793e5ee551be3a
dragonkid <idragonkid@gmail.com>
2026-05-30 22:55:20 +0800

    Revert "exp: EMA cross voter on smoothed_closes (architectural: single-voter input denoising)"
    This reverts commit 9d253c7fae1fc51e4c6f4bd5cda941a74eff81c7.

--- strategy.py
+++ strategy.py
@@ -145,7 +145,7 @@ class Strategy:
             ret_vshort = (smoothed_closes[-1] - _med_ref_short) / _med_ref_short
             ret_short = (smoothed_closes[-1] - _med_ref_med) / _med_ref_med
 
-            _ef, _es = ema(smoothed_closes[-(EMA_SLOW+10):], EMA_FAST)[-1], ema(smoothed_closes[-(EMA_SLOW+10):], EMA_SLOW)[-1]
+            _ef, _es = ema(closes[-(EMA_SLOW+10):], EMA_FAST)[-1], ema(closes[-(EMA_SLOW+10):], EMA_SLOW)[-1]
             _ret_long_lagged = (closes[-2] - closes[-LONG_WINDOW - 1]) / closes[-LONG_WINDOW - 1]
             rsi_trend_str = min(abs(_ret_long_lagged) / RSI_TREND_BIAS_DECAY, 1.0)
             _rd = np.diff(closes[-(int(round(6 + 2 * rsi_trend_str)) + 1):])
