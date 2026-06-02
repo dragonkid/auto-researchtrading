f425326f4adea0211fc52a0c2964933a45b09166
dragonkid <idragonkid@gmail.com>
2026-06-02 19:31:46 +0800

    Revert "branch step 21: revert FLIP 2.92->2.88 (hurt rally, killed flips needed for trend reversals)"
    This reverts commit 2ef61dc97d0777beee7e89c98248861a655181ef.

--- strategy.py
+++ strategy.py
@@ -81,7 +81,7 @@ MEANREV_RSI_OVERBOUGHT = 53
 # Vote / cooldown (6 voters: ret_vshort removed)
 # Continuous voting: MIN_VOTES is now a float threshold for sigmoid-weighted sums
 MIN_VOTES = 2.63
-FLIP_MIN_VOTES = 2.88
+FLIP_MIN_VOTES = 2.92
 COOLDOWN_BARS = 1
 COOLDOWN_TREND_DECAY = 0.06
 
