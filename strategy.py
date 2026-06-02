bdbfcc00a650ffb38c43256dfa1efa522ae10e14
dragonkid <idragonkid@gmail.com>
2026-06-02 17:22:47 +0800

    Revert "branch step 13: revert MACD_SIGNAL to 8 (10 too sluggish)"
    This reverts commit 6f344ded2741ca896d4b9bff5825c4d9fd1df96e.

--- strategy.py
+++ strategy.py
@@ -20,7 +20,7 @@ EMA_SLOPE_LOOKBACK = 3
 # MACD parameters
 MACD_FAST = 8
 MACD_SLOW = 16
-MACD_SIGNAL = 8  # widened from 4->6->7->8 to smooth MACD histogram further
+MACD_SIGNAL = 10  # widened from 8->10 to further smooth MACD histogram (stability)
 
 # Linear regression
 LINREG_PERIOD = 16
